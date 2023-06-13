5a103261f31addefb9b365f73b8b3befadea7128
Stardust <stardustrx0@gmail.com>
2023-06-13 22:50:19 +0700

    Lab04 (WIP 11, 12, 13)
    It's lab04 despite folder lab03 since changes are made inside the same project

--- Lab3/AimsProject/src/hust/soict/dsai/aims/Aims.java
+++ Lab3/AimsProject/src/hust/soict/dsai/aims/Aims.java
@@ -1,19 +1,23 @@
 package hust.soict.dsai.aims;
 
+import hust.soict.dsai.aims.cart.Cart;
+import hust.soict.dsai.aims.media.DigitalVideoDisc;
+import hust.soict.dsai.aims.media.Media;
+
 public class Aims
 {
     public static void main(String[] args)
     {
         Cart anOrder = new Cart();
 
-        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
-        anOrder.addDigitalVideoDisc(dvd1);
+        Media dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+        anOrder.addMedia(dvd1);
 
-        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "Georgre Lucas", 87 , 24.95f);
-        anOrder.addDigitalVideoDisc(dvd2);
+        Media dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "Georgre Lucas", 87 , 24.95f);
+        anOrder.addMedia(dvd2);
 
-        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin","Animation",18.99f);
-        anOrder.addDigitalVideoDisc(dvd3);
+        Media dvd3 = new DigitalVideoDisc("Aladin","Animation",18.99f);
+        anOrder.addMedia(dvd3);
 
         System.out.println("Total cost is: ");
         System.out.println(anOrder.totalCost());
--- Lab3/AimsProject/src/hust/soict/dsai/aims/store/Store.java
+++ Lab3/AimsProject/src/hust/soict/dsai/aims/store/Store.java
@@ -1,25 +1,29 @@
 package hust.soict.dsai.aims.store;
 
-import hust.soict.dsai.aims.disc.DigitalVideoDisc;
+import hust.soict.dsai.aims.media.DigitalVideoDisc;
+import hust.soict.dsai.aims.media.Media;
+
+import java.util.ArrayList;
 
 public class Store {
-    private DigitalVideoDisc[] itemsInStore = new DigitalVideoDisc[65536];
+    private ArrayList<Media> itemsInStore = new ArrayList<Media>();
 
-    public void addDVD(DigitalVideoDisc disc)
-    {
-        int i = itemsInStore.length;
-        itemsInStore[i-1] = disc;
-    }
-
-    public void removeDVD(DigitalVideoDisc disc) {
-        DigitalVideoDisc[] itemsInStoreRemoved = new DigitalVideoDisc[itemsInStore.length - 1];
-        int i = 0;
-        for (DigitalVideoDisc digitalVideoDisc : itemsInStore) {
-            if (digitalVideoDisc != disc) {
-                itemsInStoreRemoved[i] = digitalVideoDisc;
-                i++;
-            }
+    public void addMedia(Media media) {
+        if (itemsInStore.contains(media)) {
+            System.out.println("This media is already in the store.");
         }
-        itemsInStore = itemsInStoreRemoved;
+        else {
+            itemsInStore.add(media);
+            System.out.println("Added " + media.getTitle() + " to the store.");
+        }
+    }
+    public void removeMedia(Media media) {
+        if (itemsInStore.contains(media)) {
+            itemsInStore.remove(media);
+            System.out.println("Removed " + media.getTitle() + " from the store.");
+        }
+        else {
+            System.out.println("This media is not in the store.");
+        }
     }
 }
